928986b8ef6680a83f57ec299fb7eb2c34da3936
Julian Camilo Muñoz <juancami28@msn.com>
2015-05-27 15:46:58 -0500

    Sin imagenes, texto ingresado desde la clase

---
+++ app/src/main/java/com/example/juliancamilomunoz/antioquiaturisticav3/ItemsListActivity.java
@@ -0,0 +1,78 @@
+package com.example.juliancamilomunoz.antioquiaturisticav3;
+
+import android.content.Intent;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+
+/**
+ * Created by Xingke on 25/05/2015.
+ */
+
+public class ItemsListActivity extends FragmentActivity implements
+        ItemsListFragment.OnListItemSelectedListener{
+
+    // Flag determines if this is a one or two pane layout
+    private boolean isTwoPane = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_items_list);
+        // Call this to determine which layout we are in (tablet or phone)
+        determinePaneLayout();
+    }
+
+    private void determinePaneLayout() {
+        FrameLayout fragmentItemDetail = (FrameLayout) findViewById(R.id.flDetailContainer);
+        if (fragmentItemDetail != null) {
+            isTwoPane = true;
+            ItemsListFragment fragmentItemsList =
+                    (ItemsListFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentItemsList);
+            fragmentItemsList.setActivateOnItemClick(true);
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_items_list, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    // Handles the event when the fragment list item is selected
+    @Override
+    public void onItemSelected(Item item) {
+        if (isTwoPane) { // single activity with list and detail
+            // Replace framelayout with new detail fragment
+            ItemDetailFragment fragmentItem = ItemDetailFragment.newInstance(item);
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.flDetailContainer, fragmentItem);
+            ft.commit();
+        } else { // go to separate activity
+            // launch detail activity using intent
+            Intent i = new Intent(this, ItemDetailActivity.class);
+            i.putExtra("item", item);
+            startActivity(i);
+        }
+    }
+}
